b8f1af05c9d419cecfe36bf5258249b5ae2ebc0c
Alexander Sikomas <alexandersikomas05@gmail.com>
2025-08-12 14:58:02 +0100

    Initial commit

---
+++ src/app.rs
@@ -0,0 +1,31 @@
+pub struct App {
+    label: String,
+}
+
+impl Default for App {
+    fn default() -> Self {
+        Self {
+            label: "Hello, world!".to_owned(),
+        }
+    }
+}
+
+impl App {
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        Default::default()
+    }
+}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            egui::MenuBar::new().ui(ui, |ui| {
+                ui.menu_button("File", |ui| {
+                    if ui.button("Quit").clicked() {
+                        ctx.send_viewport_cmd(egui::ViewportCommand::Close);
+                    }
+                });
+            });
+        });
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,18 @@
+#![warn(clippy::all, rust_2018_idioms)]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+fn main() -> eframe::Result {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+
+    let native_options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([400.0, 300.0])
+            .with_min_inner_size([300.0, 220.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "eframe template",
+        native_options,
+        Box::new(|cc| Ok(Box::new(networkd_wireguard_manager::App::new(cc)))),
+    )
+}
