6a038a4e293a86b580d7e0f58b7b428c85f0d704
Alexander Sikomas <git@sikomas.com>
2025-10-15 10:01:34 +0200

    fix: removed action

--- tui/src/components/mod.rs
+++ tui/src/components/mod.rs
@@ -1,2 +1,3 @@
 pub mod connection;
+pub mod net_prompt;
 pub mod menu;
--- tui/src/network.rs
+++ tui/src/network.rs
@@ -1,4 +1,7 @@
-use com::wireless::common::AccessPoint;
+use com::{controller::Controller, wireless::common::AccessPoint};
+use tokio::sync::mpsc::{Receiver, Sender};
+
+use crate::app::AppState;
 
 #[derive(Debug)]
 pub enum NetworkAction {
@@ -17,4 +20,25 @@ pub enum NetworkUpdate {
 pub struct NetworkState {
     pub selected: Option<usize>,
     pub aps: Vec<AccessPoint>,
+}
+
+pub async fn network_handle(
+    event_rx: &mut Receiver<NetworkAction>,
+    net_state_tx: Sender<NetworkUpdate>,
+) {
+    if let Ok(mut controller) = Controller::new().await {
+        controller.determine_adapter().await;
+        while let Some(action) = event_rx.recv().await {
+            match action {
+                NetworkAction::Scan => {
+                    if let Ok(aps) = controller.scan().await {
+                        let _ = net_state_tx.send(NetworkUpdate::UpdateAps(aps)).await;
+                    }
+                }
+                NetworkAction::ForceIwd => {}
+                NetworkAction::ForceWpa => {}
+                _ => {}
+            };
+        }
+    }
 }
