b7b65c6f324753f25fe7192d392d9d6511202a70
Alexander Sikomas <115405642+alexandersikomas@users.noreply.github.com>
2025-10-30 17:11:07 +0000

    feat(wireguard): set connection ip

--- com/src/wireguard.rs
+++ com/src/wireguard.rs
@@ -1,22 +1,34 @@
+use std::{ffi::CStr, io::Cursor, net::IpAddr};
+
+use futures::stream::StreamExt;
 use neli::{
+    attr::Attribute,
     consts::{
         nl::NlmF,
-        rtnl::{Ifla, IflaInfo, Rtm},
+        rtnl::{Ifa, Ifla, IflaInfo, RtAddrFamily, Rtm},
     },
+    genl::Genlmsghdr,
     nl::{NlPayload, NlmsghdrBuilder},
-    router::asynchronous::NlRouter,
-    rtnl::{Ifinfomsg, IfinfomsgBuilder, Rtattr, RtattrBuilder},
+    router::asynchronous::{NlRouter, NlRouterReceiverHandle},
+    rtnl::{Ifaddrmsg, IfaddrmsgBuilder, Ifinfomsg, IfinfomsgBuilder, Rtattr, RtattrBuilder},
     types::{GenlBuffer, RtBuffer},
     utils::Groups,
+    ToBytes,
 };
+use tokio::process::Command;
+use tracing::info;
 
 use crate::error::ComError;
 
-struct Wireguard {}
+const INTERFACE: &str = "wg-mannd";
+
+struct Wireguard {
+    router: NlRouter,
+}
 
 impl Wireguard {
     /// Connects socket and sets up wg0
-    async fn start_interface() -> Result<(), ComError> {
+    async fn start_interface() -> Result<Self, ComError> {
         let (router, handle) =
             NlRouter::connect(neli::consts::socket::NlFamily::Route, None, Groups::empty()).await?;
 
@@ -32,7 +44,7 @@ impl Wireguard {
         attrs.push(
             RtattrBuilder::default()
                 .rta_type(Ifla::Ifname)
-                .rta_payload("wg0")
+                .rta_payload(INTERFACE)
                 .build()?,
         );
 
@@ -58,13 +70,153 @@ impl Wireguard {
             )
             .await?;
 
+        Ok(Self { router })
+    }
+
+    async fn set_conf(path: &'static str) -> Result<(), ComError> {
+        let _ = Command::new("wg")
+            .args(vec!["setconf", INTERFACE, path])
+            .output()
+            .await;
+
         Ok(())
     }
+
+    async fn set_addr(&self, ips: Vec<IpAddr>) -> Result<(), ComError> {
+        // interface index
+        let mut index: u32 = 0;
+        let ifinfomsg = IfinfomsgBuilder::default()
+            .ifi_family(neli::consts::rtnl::RtAddrFamily::Unspecified)
+            .build()?;
+
+        let mut msg: NlRouterReceiverHandle<Rtm, Ifinfomsg> = self
+            .router
+            .send(
+                Rtm::Getlink,
+                NlmF::REQUEST | NlmF::DUMP,
+                NlPayload::Payload(ifinfomsg),
+            )
+            .await?;
+
+        while let Some(Ok(res)) = msg.next::<Rtm, Ifinfomsg>().await {
+            if let Some(payload) = res.get_payload() {
+                let cur_index = payload.ifi_index();
+                for attr in res.get_payload().unwrap().rtattrs().iter() {
+                    if (*attr.rta_type() == Ifla::Ifname) {
+                        let bytes = attr.rta_payload().as_ref();
+
+                        match CStr::from_bytes_until_nul(bytes) {
+                            Ok(v) => {
+                                if (v.to_string_lossy().into_owned() == INTERFACE) {
+                                    index = cur_index.clone() as u32;
+                                    break;
+                                }
+                            }
+                            Err(e) => {}
+                        };
+                    }
+                }
+            }
+        }
+
+        println!("wg-mannd is index: {}", index);
+
+        if (index == 0) {
+            return Err(ComError::OperationFailed(
+                "Cannot find wg-mannd index!".to_string(),
+            ));
+        }
+
+        // add addr
+
+        for ip in ips {
+            match ip {
+                IpAddr::V4(addr) => {
+                    let mut attrs = RtBuffer::new();
+
+                    attrs.push(
+                        RtattrBuilder::default()
+                            .rta_type(Ifa::Local)
+                            .rta_payload(addr.to_string())
+                            .build()?,
+                    );
+
+                    let ifaddr = IfaddrmsgBuilder::default()
+                        .ifa_family(neli::consts::rtnl::RtAddrFamily::Inet)
+                        .ifa_index(index)
+                        .rtattrs(attrs)
+                        .build()?;
+
+                    self.router
+                        .send::<_, _, Rtm, Ifaddrmsg>(
+                            Rtm::Newaddr,
+                            NlmF::REQUEST | NlmF::ACK | NlmF::EXCL | NlmF::CREATE,
+                            NlPayload::Payload(ifaddr),
+                        )
+                        .await?;
+                }
+                IpAddr::V6(addr) => {
+                    let mut attrs = RtBuffer::new();
+
+                    attrs.push(
+                        RtattrBuilder::default()
+                            .rta_type(Ifa::Local)
+                            .rta_payload(addr.to_string())
+                            .build()?,
+                    );
+
+                    let ifaddr = IfaddrmsgBuilder::default()
+                        .ifa_family(neli::consts::rtnl::RtAddrFamily::Inet6)
+                        .ifa_index(index)
+                        .rtattrs(attrs)
+                        .build()?;
+
+                    self.router
+                        .send::<_, _, Rtm, Ifaddrmsg>(
+                            Rtm::Newaddr,
+                            NlmF::REQUEST | NlmF::ACK | NlmF::EXCL | NlmF::CREATE,
+                            NlPayload::Payload(ifaddr),
+                        )
+                        .await?;
+                }
+            }
+        }
+        Ok(())
+    }
+
+    async fn set_mtu(mtu: u32) -> Result<(), ComError> {
+        todo!()
+    }
+
+    // [#] resolvconf -a il-tlv-wg-102 -m 0 -x
+    // resolvconf: signature mismatch: /etc/resolv.conf
+    // resolvconf: run `resolvconf -u` to update
 }
 
 // tests
-#[tokio::test]
-async fn init_wg_test() -> Result<(), ComError> {
-    Wireguard::start_interface().await?;
-    Ok(())
+mod tests {
+    use std::sync::Arc;
+
+    use tokio::sync::OnceCell;
+
+    use super::*;
+
+    static WIREGUARD_INTERFACE: OnceCell<Arc<Wireguard>> = OnceCell::const_new();
+
+    async fn get_wg_interface() -> Result<Arc<Wireguard>, ComError> {
+        WIREGUARD_INTERFACE
+            .get_or_try_init(|| async {
+                let interface = Wireguard::start_interface().await?;
+                Ok(Arc::new(interface))
+            })
+            .await
+            .map(|arc| arc.clone())
+    }
+
+    #[tokio::test]
+    async fn set_addr_test() -> Result<(), ComError> {
+        let wg = get_wg_interface().await?;
+        wg.set_addr(vec![]).await?;
+        Ok(())
+    }
 }
