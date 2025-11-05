0eeced26bfa7ee77de3ec9b7b40647abe8e897e7
Alexander Sikomas <115405642+alexandersikomas@users.noreply.github.com>
2025-11-05 09:30:25 +0000

    feat: wireguard inteface and wpa_supplicant scan
    Deleted the wireguard interface in the test to fix
    network issues that were caused do to it being set
    as default.
    
    Started fleshing out wpa_supplicant

--- com/src/wireless/wpa_supplicant.rs
+++ com/src/wireless/wpa_supplicant.rs
@@ -1,19 +1,25 @@
+//! Reference: https://w1.fi/wpa_supplicant/devel/dbus.html#dbus_network
+
 use async_trait::async_trait;
-use zbus::Connection;
+use zbus::{Connection, Proxy};
 
 use crate::{
     error::ComError,
-    wireless::{common::Security, WifiAdapter},
+    wireless::{
+        common::{get_prop_from_proxy, Security},
+        WifiAdapter,
+    },
 };
 
 #[derive(Debug, Clone)]
-pub struct WpaSupplicant {}
+pub struct WpaSupplicant {
+    conn: Connection,
+    service: String,
+    path: String,
+}
 
 #[async_trait]
 impl WifiAdapter for WpaSupplicant {
-    // async fn new(conn: Connection) -> Result<Self, ComError> {
-    //     todo!()
-    // }
     async fn connect_network(
         &self,
         ssid: String,
@@ -37,7 +43,38 @@ impl WifiAdapter for WpaSupplicant {
 }
 
 impl WpaSupplicant {
-    pub async fn new(conn: Connection) -> Result<Self, ComError> {
-        todo!()
+    pub fn new(conn: Connection) -> Result<Self, ComError> {
+        let service = String::from("fi.w1.wpa_supplicant1");
+        let path = String::from("/fi/w1/wpa_supplicant1");
+        Ok(Self {
+            conn,
+            service,
+            path,
+        })
+    }
+
+    pub async fn scan(&self) -> Result<(), ComError> {
+        let proxy = Proxy::new(
+            &self.conn,
+            self.service.clone(),
+            self.path.clone(),
+            format!("{}.Interface", self.service.clone()),
+        )
+        .await?;
+
+        proxy.call::<_, _, ()>("Scan", &()).await?;
+        Ok(())
+    }
+}
+
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_wpa_scan() -> Result<(), ComError> {
+        let conn = Connection::system().await.unwrap();
+        let wpa = WpaSupplicant::new(conn)?;
+        wpa.scan().await?;
+        Ok(())
     }
 }
