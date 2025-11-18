137bfa83093fd588563485e8e94061f2ecc212f9
Alexander Sikomas <115405642+alexandersikomas@users.noreply.github.com>
2025-11-18 10:22:26 +0000

    feat: convert signal vec to SelectAll

--- com/src/state/signals.rs
+++ com/src/state/signals.rs
@@ -1,4 +1,7 @@
-use futures::StreamExt;
+use futures::{
+    stream::{select_all, SelectAll},
+    StreamExt,
+};
 use tokio::sync::mpsc::{Receiver, Sender};
 use tracing::info;
 use zbus::{proxy::SignalStream, Message};
@@ -6,18 +9,19 @@ use zbus::{proxy::SignalStream, Message};
 use crate::state::network::NetworkAction;
 
 pub struct SignalManager<'a> {
-    pub signals: Vec<SignalStream<'a>>,
+    pub signals: SelectAll<SignalStream<'a>>,
 }
 
 pub enum SignalUpdate<'a> {
     Add(SignalStream<'a>),
-    Remove(SignalStream<'a>),
     Clear,
 }
 
 impl<'a> SignalManager<'a> {
     pub fn new() -> Self {
-        Self { signals: vec![] }
+        Self {
+            signals: select_all(Vec::<SignalStream<'a>>::new()),
+        }
     }
 
     pub fn handle_update(&mut self, update: SignalUpdate<'a>) {
@@ -25,31 +29,17 @@ impl<'a> SignalManager<'a> {
             SignalUpdate::Add(stream) => {
                 self.signals.push(stream);
             }
-            SignalUpdate::Remove(stream) => {
-                self.signals.retain(|v| v.name() != stream.name());
-            }
             SignalUpdate::Clear => {
                 self.signals.clear();
             }
         }
     }
 
-    pub async fn recv(&mut self, tx: Sender<NetworkAction>) {
-        let mut messages = vec![];
-        for signal in &mut self.signals {
-            while let Some(msg) = signal.next().await {
-                info!("The signal recieved was: {:?}", msg);
-                messages.push(msg);
-            }
-        }
-        if !messages.is_empty() {
-            self.process_messages(messages, tx);
-        }
+    pub async fn recv(&mut self) -> Option<Message> {
+        self.signals.next().await
     }
 
-    pub async fn process_messages(&self, messages: Vec<Message>, tx: Sender<NetworkAction>) {
-        for msg in messages {
-            info!("{msg}");
-        }
+    pub async fn process_messages(&self, message: Message, tx: Sender<NetworkAction>) {
+        info!("Processing");
     }
 }
